0ea1dc01557f8fd44d2614db8eda163bed014c5a
Rock Neurotiko <miguelglafuente@gmail.com>
2015-07-30 12:35:37 +0200

    getUpdates take 3 optional parameters: - Offset: This is have to be the last update_id + 1, default no offset - Limit: 1 to 100, default 100 - Timeout: To allow long-polling, you can set 1-20 seconds to wait until the response, this is really great to emulate push notifications. :heart:

--- lib/yocingo.ex
+++ lib/yocingo.ex
@@ -2,7 +2,7 @@ defmodule Yocingo do
   # The telegram bot api URL
   @api_url "https://api.telegram.org/bot"
 
-  # This function reads the token from the file called token 
+  # This function reads the token from the file called token
   def get_token do
     {ret, body} = File.read "token"
 
@@ -10,12 +10,12 @@ defmodule Yocingo do
       :ok -> String.strip body, ?\n
       :error -> raise File, message: "File Token not found"
     end
-    
+
   end
 
   # Creates the proper API method URL
   def build_url(method) do
-    @api_url <> get_token <> "/" <>method 
+    @api_url <> get_token <> "/" <>method
   end
 
   # Obtains and parses a petition
@@ -29,7 +29,7 @@ defmodule Yocingo do
         {:ok, %HTTPoison.Response{status_code: 200, body: body}} =
         HTTPoison.post((build_url method), request)
         body |> JSX.decode!
-    end 
+    end
   end
 
   # TELEGRAM API BOT FUCNTIONS
@@ -42,10 +42,15 @@ defmodule Yocingo do
     "getUpdates" |> get_response
   end
 
+  def get_updates(offset \\ 0, limit \\ 100, timeout \\ 20) do
+    body = {:form, [offset: offset, limit: limit, timeout: timeout]}
+    get_response("getUpdates", body)
+  end
+
   def send_message(chat_id, text, disable_web_page_preview \\ false,
       reply_to_mensaje_id \\ nil, reply_markup \\ nil) do
     body = {:form,[chat_id: chat_id, text: text]}
     get_response("sendMessage", body)
   end
-  
+
 end
