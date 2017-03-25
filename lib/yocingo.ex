301636995ce35a15ae823e9d6f9997cdcebf32e8
PraiseGeek <praisegeek@gmail.com>
2017-03-25 15:55:03 +0100

    Updated parse_mode

--- lib/yocingo.ex
+++ lib/yocingo.ex
@@ -1,13 +1,13 @@
 defmodule Yocingo do
 
   @doc """
-  
+
   This is a full Telegram Bot API. With this module you can
   create your own Telegram Bot.
 
   Yawolf
   https://github.com/Yawolf/yocingo
-  
+
   """
   # The telegram bot api URL
   @api_url "https://api.telegram.org/bot"
@@ -45,19 +45,19 @@ defmodule Yocingo do
   # TELEGRAM API BOT FUCNTIONS
 
   @doc """
-  A simple method for testing your bot's auth token. 
-  Requires no parameters. 
+  A simple method for testing your bot's auth token.
+  Requires no parameters.
   Returns basic information about the bot in form of a HashMap.
 
   ## Arguments
-    - offset: Integer, Optional, Identifier of the first update to be returned. 
-    Must be greater by one than the highest among the identifiers of previously 
+    - offset: Integer, Optional, Identifier of the first update to be returned.
+    Must be greater by one than the highest among the identifiers of previously
     received updates. By default, updates starting with the earliest unconfirmed
     update are returned. An update is considered confirmed as soon as getUpdates
     is called with an offset higher than its update_id.
-    - limits: Integer, Optional, Limits the number of updates to be retrieved. 
+    - limits: Integer, Optional, Limits the number of updates to be retrieved.
     Values between 1—100 are accepted. Defaults to 100
-    - timeout: Integer, Optional, Timeout in seconds for long polling. 
+    - timeout: Integer, Optional, Timeout in seconds for long polling.
     Defaults to 0, i.e. usual short polling
   """
   def get_me do
@@ -68,15 +68,15 @@ defmodule Yocingo do
   Use this method to receive incoming updates using long polling.
   Returns the updates in form of a HashMap
 
-  ## Arguments  
-    - offset: Integer, Optional, Identifier of the first update to be returned. 
-    Must be greater by one than the highest among the identifiers of previously 
+  ## Arguments
+    - offset: Integer, Optional, Identifier of the first update to be returned.
+    Must be greater by one than the highest among the identifiers of previously
     received updates. By default, updates starting with the earliest unconfirmed
     update are returned. An update is considered confirmed as soon as getUpdates
     is called with an offset higher than its update_id.
-    - limits: Integer, Optional, Limits the number of updates to be retrieved. 
+    - limits: Integer, Optional, Limits the number of updates to be retrieved.
     Values between 1—100 are accepted. Defaults to 100
-    - timeout: Integer, Optional, Timeout in seconds for long polling. 
+    - timeout: Integer, Optional, Timeout in seconds for long polling.
     Defaults to 0, i.e. usual short polling
   """
 
@@ -90,21 +90,22 @@ defmodule Yocingo do
   Retruns information in form of a HashMap.
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
     - text: String, Required, Text of the message to be sent.
-    - disable_web_page_preview: Boolean, Optional, Disables link previews for 
+    - disable_web_page_preview: Boolean, Optional, Disables link previews for
     links in this message.
-    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the 
+    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the
     original message.
-    - reply_markup: Dict of List of List, Optional, Additional interface options. 
-    A JSON-serialized object for a custom reply keyboard, instructions to hide 
+    - reply_markup: Dict of List of List, Optional, Additional interface options.
+    A JSON-serialized object for a custom reply keyboard, instructions to hide
     keyboard or to force a reply from the user. (Check Telegram API for more Information).
   """
 
-  def send_message(chat_id, text, disable_web_page_preview \\ false,
+  def send_message(chat_id, parse_mode \\ nil, text, disable_web_page_preview \\ false,
                    reply_to_mensaje_id \\ nil, reply_markup \\ %{"reply_markup" => []}) do
     body = {:form,[chat_id: chat_id,
+                   parse_mode: parse_mode,
                    text: text,
                    disable_web_page_preview: disable_web_page_preview,
                    reply_to_mensaje_id: reply_to_mensaje_id,
@@ -121,7 +122,7 @@ defmodule Yocingo do
   ## Arguments
     - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
-    - from_chat_id: Integer, Required, Unique identifier for the chat where 
+    - from_chat_id: Integer, Required, Unique identifier for the chat where
     the original message was sent, user or group.
     - message_id: Unique message identifier.
   """
@@ -138,20 +139,20 @@ defmodule Yocingo do
   Returns info in form of a HashMap
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
-    - photo: [File | String], Required, Photo to send. You can either pass a 
-    file_id as String to resend a photo that is already on the Telegram servers, 
+    - photo: [File | String], Required, Photo to send. You can either pass a
+    file_id as String to resend a photo that is already on the Telegram servers,
     or upload a new photo using multipart/form-data.
     - caption: String, Optional, Photo caption (may also be used when resending
     photos by file_id).
-    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the 
+    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the
     original message.
-    - reply_markup: Dict of List of List, Optional, Additional interface options. 
-    A JSON-serialized object for a custom reply keyboard, instructions to hide 
+    - reply_markup: Dict of List of List, Optional, Additional interface options.
+    A JSON-serialized object for a custom reply keyboard, instructions to hide
     keyboard or to force a reply from the user. (Check Telegram API for more Information).
   """
-  
+
   def send_photo(chat_id, photo, caption \\ :nil,
                  reply_to_message_id \\ :nil, reply_markup \\ :nil) do
     body = build_file({:photo, photo}, [chat_id: chat_id, caption: caption, reply_to_message_id: reply_to_message_id], reply_markup)
@@ -163,16 +164,16 @@ defmodule Yocingo do
   Returns info in form of a HashMap
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
-    - audio: [File | String], Required, Audio file to send. You can either pass a 
-    file_id as String to resend a audio file that is already on the Telegram servers, 
+    - audio: [File | String], Required, Audio file to send. You can either pass a
+    file_id as String to resend a audio file that is already on the Telegram servers,
     or upload a new photo using multipart/form-data.
     - duration: Integer, Optional, Duration of sent audio in seconds
-    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the 
+    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the
     original message.
-    - reply_markup: Dict of List of List, Optional, Additional interface options. 
-    A JSON-serialized object for a custom reply keyboard, instructions to hide 
+    - reply_markup: Dict of List of List, Optional, Additional interface options.
+    A JSON-serialized object for a custom reply keyboard, instructions to hide
     keyboard or to force a reply from the user. (Check Telegram API for more Information).
   """
 
@@ -187,18 +188,18 @@ defmodule Yocingo do
   Returns info in form of a HashMap
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
-    - document: [File | String], Required, File to send. You can either pass a 
-    file_id as String to resend a document that is already on the Telegram servers, 
+    - document: [File | String], Required, File to send. You can either pass a
+    file_id as String to resend a document that is already on the Telegram servers,
     or upload a new photo using multipart/form-data.
-    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the 
+    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the
     original message.
-    - reply_markup: Dict of List of List, Optional, Additional interface options. 
-    A JSON-serialized object for a custom reply keyboard, instructions to hide 
-    keyboard or to force a reply from the user. (Check Telegram API for more Information).  
+    - reply_markup: Dict of List of List, Optional, Additional interface options.
+    A JSON-serialized object for a custom reply keyboard, instructions to hide
+    keyboard or to force a reply from the user. (Check Telegram API for more Information).
   """
-  
+
   def send_document(chat_id, document, reply_to_message_id \\ :nil,
                     reply_markup \\ :nil) do
     body = build_file({:document, document}, [chat_id: chat_id, reply_to_message_id: reply_to_message_id], reply_markup)
@@ -210,16 +211,16 @@ defmodule Yocingo do
   Returns info in form of a HashMap
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
-    - sticker: [File | String], Required, Sticker to send. You can either pass a 
-    file_id as String to resend a sticker that is already on the Telegram servers, 
+    - sticker: [File | String], Required, Sticker to send. You can either pass a
+    file_id as String to resend a sticker that is already on the Telegram servers,
     or upload a new photo using multipart/form-data.
-    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the 
+    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the
     original message.
-    - reply_markup: Dict of List of List, Optional, Additional interface options. 
-    A JSON-serialized object for a custom reply keyboard, instructions to hide 
-    keyboard or to force a reply from the user. (Check Telegram API for more Information).  
+    - reply_markup: Dict of List of List, Optional, Additional interface options.
+    A JSON-serialized object for a custom reply keyboard, instructions to hide
+    keyboard or to force a reply from the user. (Check Telegram API for more Information).
   """
 
   def send_sticker(chat_id, sticker, reply_to_message_id \\ :nil,
@@ -233,18 +234,18 @@ defmodule Yocingo do
   Returns info in form of a HashMap
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
-    - video: [File | String], Required, Video to send. You can either pass a 
-    file_id as String to resend a video that is already on the Telegram servers, 
+    - video: [File | String], Required, Video to send. You can either pass a
+    file_id as String to resend a video that is already on the Telegram servers,
     or upload a new photo using multipart/form-data.
-    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the 
+    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the
     original message.
-    - reply_markup: Dict of List of List, Optional, Additional interface options. 
-    A JSON-serialized object for a custom reply keyboard, instructions to hide 
-    keyboard or to force a reply from the user. (Check Telegram API for more Information).  
+    - reply_markup: Dict of List of List, Optional, Additional interface options.
+    A JSON-serialized object for a custom reply keyboard, instructions to hide
+    keyboard or to force a reply from the user. (Check Telegram API for more Information).
   """
-  
+
   def send_video(chat_id, video, duration \\ 0, reply_to_message_id \\ :nil,
                  reply_markup \\ :nil) do
     body = build_file({:video, video}, [chat_id: chat_id, duration: duration, reply_to_message_id: reply_to_message_id], reply_markup)
@@ -252,16 +253,16 @@ defmodule Yocingo do
   end
 
   @doc """
-  Use this method when you need to tell the user that something is happening on the 
+  Use this method when you need to tell the user that something is happening on the
   bot's side. The status is set for 5 seconds or less (when a message arrives from
   your bot, Telegram clients clear its typing status).
   Returns info in form of a HashMap.
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
     - action: String, Required, Type of action to broadcast. Choose one, depending
-    on what the user is about to receive: typing for text messages, upload_photo 
+    on what the user is about to receive: typing for text messages, upload_photo
     for photos, record_video or upload_video for videos, record_audio or upload_audio
     for audio files, upload_document for general files, find_location for location data.
   """
@@ -279,7 +280,7 @@ defmodule Yocingo do
     - user_id: Integer, Required, Unique identifier of the target user.
     - offset: Integer, Optional, Sequential number of the first photo to be returned.
     By default the value is 0 (means all).
-    - limit: Integer, Optional, Limits the number of photos to be retrieved. 
+    - limit: Integer, Optional, Limits the number of photos to be retrieved.
     Values between 1—100 are accepted. Defaults to 100.
   """
 
@@ -295,14 +296,14 @@ defmodule Yocingo do
   Returns info in form of a HashMap.
 
   ## Arguments
-    - chat_id: Integer, Required, Unique identifier for the message recipient, 
+    - chat_id: Integer, Required, Unique identifier for the message recipient,
     user or group.
     - latitude: Float, Required, Latitude of location.
     - longitude: Float, Required, Longitude of location.
-    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the 
+    - reply_to_message_id: Integer, Optional, If the message is a reply, ID of the
     original message.
-    - reply_markup: Dict of List of List, Optional, Additional interface options. 
-    A JSON-serialized object for a custom reply keyboard, instructions to hide 
+    - reply_markup: Dict of List of List, Optional, Additional interface options.
+    A JSON-serialized object for a custom reply keyboard, instructions to hide
     keyboard or to force a reply from the user. (Check Telegram API for more Information).
   """
 
